7752a59987373bb91f7ad527064296b35be95de0
Anton Lukin <anton@lukin.me>
2024-04-19 12:17:57 +0300

    add share-stats helper

---
+++ 52.share-stats.php
@@ -0,0 +1,115 @@
+<?php
+
+if(php_sapi_name() !== 'cli') {
+    exit;
+}
+
+$_SERVER = [
+    "SERVER_PROTOCOL" => "HTTP/1.1",
+    "HTTP_HOST"       => "knife.media",
+    "SERVER_NAME"     => "knife.media",
+    "REQUEST_URI"     => "/",
+    "REQUEST_METHOD"  => "GET"
+];
+
+define('WP_CACHE', false);
+define('WP_DEBUG', true);
+define('WP_USE_THEMES', false);
+
+require( __DIR__ . '/../wordpress/wp-load.php');
+
+function find_posts() {
+    global $wpdb;
+
+    $conf = wp_parse_args(
+        KNIFE_ANALYTICS,
+        array(
+            'host'     => DB_HOST,
+            'name'     => DB_NAME,
+            'user'     => DB_USER,
+            'password' => DB_PASSWORD,
+        )
+    );
+
+    // Create custom db connection
+    $db = new wpdb( $conf['user'], $conf['password'], $conf['name'], $conf['host'] );
+
+    $posts = $db->get_results(
+        "SELECT post_id, slug, pageviews, publish, fb, vk FROM posts
+        LEFT JOIN shares USING (post_id)
+        LEFT JOIN views USING (post_id)",
+        ARRAY_A
+    );
+
+    usort($posts, function($a, $b) {
+        return $b['pageviews'] <=> $a['pageviews'];
+    });
+
+    $count = 0;
+
+    foreach ($posts as $post) {
+        if ($count++ > 50) {
+            break;
+        }
+
+        if (empty(get_permalink($post['post_id']))) {
+            continue;
+        }
+
+        printf(
+            "<a href='%s'>%s</a> - Views: %s<br>",
+            get_permalink($post['post_id']),
+            get_the_title($post['post_id']),
+            $post['pageviews']
+        );
+    }
+
+    usort($posts, function($a, $b) {
+        return $b['fb'] <=> $a['fb'];
+    });
+
+    $count = 0;
+
+    foreach ($posts as $post) {
+        if ($count++ > 50) {
+            break;
+        }
+
+        if (empty(get_permalink($post['post_id']))) {
+            continue;
+        }
+
+        printf(
+            "<a href='%s'>%s</a> - Facebook: %s<br>",
+            get_permalink($post['post_id']),
+            get_the_title($post['post_id']),
+            $post['fb']
+        );
+    }
+
+    usort($posts, function($a, $b) {
+        return $b['vk'] <=> $a['vk'];
+    });
+
+    $count = 0;
+
+    foreach ($posts as $post) {
+        if ($count++ > 50) {
+            break;
+        }
+
+        if (empty(get_permalink($post['post_id']))) {
+            continue;
+        }
+
+        printf(
+            "<a href='%s'>%s</a> - VK: %s<br>",
+            get_permalink($post['post_id']),
+            get_the_title($post['post_id']),
+            $post['vk']
+        );
+    }
+}
+
+
+find_posts();
